3037c56add324f88080ed8c95b036382c30d5739
penach0 <josemanuel.penacho@gmail.com>
2022-08-25 19:46:53 +0100

    Add event listener to equal sign
    -Limited functionality: only works for a single operation

--- calculator.js
+++ calculator.js
@@ -1,4 +1,4 @@
-const add = (a, b) => a + b;
+const add = (a, b) => +a + +b;
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
@@ -9,29 +9,58 @@ let firstOperand;
 let secondOperand;
 
 const display = document.querySelector('#display');
-
+console.log(display);
 const nums = [...document.getElementsByClassName('number')];
 nums.forEach(num => num.addEventListener('click', numbers));
 
 const operators = [...document.getElementsByClassName('operator')];
 operators.forEach(operator => operator.addEventListener('click', storeValues))
 
+const equal = document.querySelector('.equal');
+console.log(equal);
+equal.addEventListener('click', e =>
+    display.textContent = operate(firstOperand, displayValue, operator));
+
 function storeValues(e){
     operator = e.target.textContent;
-    firstOperand = displayValue;
+    firstOperand = displayValue; 
+    display.textContent = '';
 }
 
 function numbers(e){
     display.textContent += e.target.textContent;
     displayValue = display.textContent;
+    /* if(operator) {
+        display.textContent = '';
+    } */
 }
 
 
 function operate(a, b, operation){
+    console.log(a);
+    console.log(b);
+    console.log(operation);
     switch(operation){
         case '+': return add(a,b);
         case '-': return subtract(a,b);
         case '*': return multiply(a,b);
         case '/': return divide(a,b);
     };
+    
+
+
+    /* switch(operation){
+        case '+':
+            display.textContent = add(a,b);
+            break;
+        case '-':
+            display.textContent = subtract(a,b);
+            break;
+        case '*':
+            display.textContent = multiply(a,b);
+            break;
+        case '/':
+            display.textContent = divide(a,b);
+            break;
+    }; */
 }
