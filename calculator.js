35c081b49064f323ccf366434a199e7a4aa12897
penach0 <josemanuel.penacho@gmail.com>
2022-08-25 17:14:15 +0100

    Create function to add numbers clicked to the display

--- calculator.js
+++ calculator.js
@@ -3,11 +3,21 @@ const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
 
+const display = document.querySelector('#display');
+const nums = [...document.getElementsByClassName('number')];
+nums.forEach(num => num.addEventListener('click', numbers));
+
+function numbers(e){
+    console.log(e);
+    display.textContent += e.target.textContent;
+}
+
+
 function operate(a, b, operation){
     switch(operation){
         case '+': return add(a,b);
         case '-': return subtract(a,b);
         case '*': return multiply(a,b);
         case '/': return divide(a,b);
-    }
+    };
 }
