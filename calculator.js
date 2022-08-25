7c44bf1b75ddc5ab1d92b0313cec064d48ef6e59
penach0 <josemanuel.penacho@gmail.com>
2022-08-25 17:37:56 +0100

    Add ways to store values
    -Add storeValues function to store displayValues and the operand
    -Create variables to store the necessary values

--- calculator.js
+++ calculator.js
@@ -3,13 +3,27 @@ const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
 
+let displayValue;
+let operator;
+let firstOperand;
+let secondOperand;
+
 const display = document.querySelector('#display');
+
 const nums = [...document.getElementsByClassName('number')];
 nums.forEach(num => num.addEventListener('click', numbers));
 
+const operators = [...document.getElementsByClassName('operator')];
+operators.forEach(operator => operator.addEventListener('click', storeValues))
+
+function storeValues(e){
+    operator = e.target.textContent;
+    firstOperand = displayValue;
+}
+
 function numbers(e){
-    console.log(e);
     display.textContent += e.target.textContent;
+    displayValue = display.textContent;
 }
 
 
