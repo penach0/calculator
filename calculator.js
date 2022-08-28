5c30c44c54eb97dad83922a78a8f5c797344ead1
penach0 <josemanuel.penacho@gmail.com>
2022-08-28 22:18:29 +0100

    Add keyboard support

--- calculator.js
+++ calculator.js
@@ -17,34 +17,69 @@ const secondOperand = document.createElement('p');
 
 const nums = [...document.getElementsByClassName('number')];
 nums.forEach(num => num.addEventListener('click', numbers));
+document.addEventListener('keydown', e => {
+ if(!isNaN(e.key)) numbers(e);
+});
 
 const operators = [...document.getElementsByClassName('operator')];
 operators.forEach(operator => operator.addEventListener('click', storeValues));
+document.addEventListener('keydown', e => {
+    if(['+','-','*','/','Enter'].includes(e.key)) storeValues(e);
+});
 
 const allClear = document.querySelector('.clear');
 allClear.addEventListener('click', clear);
+document.addEventListener('keydown', e => {
+    if(e.key === 'Delete') clear(e);
+})
 
 const float = document.querySelector('.float');
 float.addEventListener('click', decimalPoint);
+document.addEventListener('keydown', e => {
+    if(e.key === '.') decimalPoint(e);
+})
 
 const backSpace = document.querySelector('.backspace');
 backSpace.addEventListener('click', clearOne);
+document.addEventListener('keydown', e => {
+    if(e.key === 'Backspace') clearOne(e);
+})
 
+/* const keyboardSupport = document.addEventListener('keydown', e => {
+    switch(e.key){
+        case !isNaN(e.key):
+            numbers(e);
+            break;
+        case ['+','-','*','/','Enter'].includes(e.key):
+            storeValues(e);
+            break;
+        case (e.key === 'Delete'):
+            clear(e);
+            break;
+        case (e.key === '.'):
+            decimalPoint(e)
+            break;
+        case (e.key === 'Backspace'):
+            clearOne(e);
+            break;
+        default: return;
+    }
+}) */
 
 function numbers(e){
     removePressed();
     if (!operator) {
         if(firstOperand.textContent.length < maxSize){
-            firstOperand.textContent += e.target.textContent;
+            eventType(e, firstOperand);
             display.appendChild(firstOperand);
         }
     } else if(operator === '=' && storedValue){
         clear(e);
-        firstOperand.textContent += e.target.textContent;
+        eventType(e, firstOperand);
         display.appendChild(firstOperand);
     } else{
         if(secondOperand.textContent.length < maxSize){
-            secondOperand.textContent += e.target.textContent;
+            eventType(e, secondOperand);
             if(display.contains(firstOperand)) display.replaceChild(secondOperand, firstOperand);
         }
     }   
@@ -52,14 +87,25 @@ function numbers(e){
 
 function storeValues(e){
     removePressed();
-    e.target.classList.add('pressed');
+    if(e.target.classList.contains('operator')) e.target.classList.add('pressed');
+    if(e.type === 'keydown') operators.forEach((operator) => {
+        if(e.key === operator.textContent) operator.classList.add('pressed');
+        if(e.key === 'Enter') document.querySelector('.equal').classList.add('pressed');
+    })
+
     if(!firstOperand.textContent && !secondOperand.textContent) return;
     if(!storedValue){
         storedValue = firstOperand.textContent;
     } else if(secondOperand.textContent){
         operate(storedValue, secondOperand.textContent, operator);
     }
-    operator = e.target.textContent;
+
+    if(e.type === 'click') operator = e.target.textContent;
+    if(e.type === 'keydown'){
+        if(e.key === 'Enter'){ 
+            operator = '='
+        }else operator = e.key;
+    }
 }
 
 function operate(a, b, operation){
@@ -102,9 +148,7 @@ function clear(e){
 function clearOne(e){
     if(!display.firstChild) return;
     display.firstChild.textContent = display.firstChild.textContent.slice(0, -1);
-    operator = ''
-    storedValue = '';
-    removePressed();
+    if(display.firstChild.textContent === '') clear(e);
 }
 
 function decimalSize(string){
@@ -123,4 +167,9 @@ function removePressed(){
     operators.forEach((operator) => {
         if(operator.classList.contains('pressed')) operator.classList.remove('pressed')
     });
+}
+
+function eventType(e, element){
+    if(e.type === 'keydown') element.textContent += e.key;
+    if(e.type === 'click') element.textContent += e.target.textContent;
 }
